8db824bd7cc4bbb89d955c8ac0071ebc8d2ab824
Audrius <audriusprivate@gmail.com>
2017-03-17 14:19:55 +0200

    Smulkus pataisymai.

--- if/7.cpp
+++ if/7.cpp
@@ -17,26 +17,22 @@ int main() {
 
   if(a > 0 && b > 0 && c > 0)
   {
-      d = (b ^ 2) - (4 * a * c); // diskriminanto formule
+      d = (b * b) - (4 * a * c); // diskriminanto formule
       cout << "Diskriminantas:  " << d << endl;
-      if(d > 0) // jeigu diskriminantas didesnis uz nuli..
-      {
-          cout << "2 sprendiniai" << endl;
+    
+      if(d > 0) {
+          cout << "Lygtis turi du sprendinius" << endl;
           x1 = (-b + sqrt(d)) / (2 * a); // x1
           x2 = (-b - sqrt(d)) / (2 * a); // x2
           cout << "x1 = " << x1 << endl;
           cout << "x2 = " << x2 << endl;
       }
-          else if(d == 0)
-      {
-          cout << "Lygtis turi du sprendinius:" << endl;
+    
+      if(d == 0) {
+          cout << "Lygtis turi 1 sprendini:" << endl;
           x1 = (-b / (2 * a));
-          cout << "x1 = " << x1 << endl;
+          cout << "x = " << x1 << endl;
       }
-      else
-      {
-          cout << "Lygtis sprendiniu neturi." << endl;
-      }
-      cout << '\n' << endl;
+      else cout << "Lygtis sprendiniu neturi." << endl;
   }
 }
